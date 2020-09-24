428e737518156f73b6d324dad26ab1ec0768b724
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2020-09-24 08:27:28 -0400

    argparse to solve #1

--- main.py
+++ main.py
@@ -1,46 +1,29 @@
+#!/usr/bin/env python
+from CluStream import CluStream
+
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
-
-from CluStream import CluStream
-import cProfile, pstats, io
-
-
-
-def profile(fnc):
-	
-	"""A decorator that uses cProfile to profile a function"""
-	
-	def inner(*args, **kwargs):
-		
-		pr = cProfile.Profile()
-		pr.enable()
-		retval = fnc(*args, **kwargs)
-		pr.disable()
-		s = io.StringIO()
-		sortby = 'cumulative'
-		ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-		ps.print_stats()
-		print(s.getvalue())
-		return retval
-
-	return inner
-# @profile
 from time import time
-def main():
-	model=CluStream(m=2000,h=1000)
+import argparse
+
+parser=argparse.ArgumentParser()
+parser.add_argument("dataset",help=".csv dataset to test")
+parser.add_argument("-c","--chunksize",type=int,default=2000)
+parser.add_argument("-m","--microclusters",type=int,default=2000)
+parser.add_argument("-H","--window-range",type=int,default=1000)
+args=parser.parse_args()
+
+
+
+if __name__=="__main__":
+	model=CluStream(m=args.microclusters,h=args.window_range)
 	t=0
 	total_time=0
-	for chunk in tqdm(pd.read_csv("only_numerical.csv",chunksize=2000,dtype=np.float32),total=4898431//2000,disable=True):
-		
-		for datapoint in tqdm(chunk.values,disable=True):
+	for chunk in pd.read_csv(args.dataset,chunksize=args.chunksize,dtype=np.float32):
+		for datapoint in chunk.values:
 			start=time()
 			model.offline_cluster(datapoint,t)
-			if t>model.m:
+			if t>model.m: #ignore initialization
 				total_time+=1//(time()-start)
 			t+=1
-		if t==4000:
-			break
-	print(total_time/((t-model.m)),"avg pps")
-		
-main()
+	print(total_time/((t-model.m)),"avg points per second")
